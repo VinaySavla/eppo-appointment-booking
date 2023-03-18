78e702fd63cece91f4f0d573e09e726a661e5cca
Kenneth <Kenneth012004@outlook.com>
2023-03-18 15:17:19 +0530

    Login Signup validation

--- src/components/login.js
+++ src/components/login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import Navbar from "./navbar";
 import FooterAS from "./footer";
 import homework from "../assets/Home.jpg";
@@ -9,6 +9,69 @@ import Formlabel from "../subcomponents/formlabel";
 import Inputbox from "../subcomponents/inputbox";
 
 function Login(props) {
+    function reducer(currentState, action) {
+
+        switch (action.type) {
+            case "email":
+                return { ...currentState, email: action.value };
+            case "password":
+                return { ...currentState, password: action.value }
+            case "alerttxt":
+                return { ...currentState, alerttxt: action.value };
+        }
+
+        return currentState;
+    }
+
+    function updateEmail(email) {
+        dispatch({ type: "email", value: email.target.value });
+    }
+
+    function updatePassword(password) {
+        dispatch({ type: "password", value: password.target.value });
+    }
+    function updateAlert(alerttxt){
+        dispatch({type : 'alerttxt', value: alerttxt});
+    }
+
+    const [state, dispatch] = useReducer(reducer, { password: '', email: '', alerttxt: '', });
+
+
+    const submitHandler = async (e) => {
+
+    if(!(String(state.email)
+            .toLowerCase()
+            .match(
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            ))) {
+            updateAlert("Please enter a valid email address!");
+        }
+    else if(state.password === '')
+    {
+        updateAlert("Password cannot be blank!");
+    }
+        else
+        {
+            updateAlert("");
+            // const email = state.email;
+            // const password = state.password;
+            // try {
+            //
+            //     e.preventDefault();
+            //     const response = await login({ email , password });
+            //     if (response.success) {
+            //         console.log(response.message);
+            //         await AsyncStorage.setItem("token", response.data);
+            //         navigation.navigate("Home");
+            //     } else {
+            //         updateAlert("Invalid Login Details!");
+            //     }
+            // } catch (error) {
+            //     console.log(error.message);
+            // }
+        }
+
+    };
     return (
         <>
             <Navbar/>
@@ -24,18 +87,23 @@ function Login(props) {
                                     <Formlabel text='Email Address'/>
                                     <Inputbox type="email"
                                               className="form-control"
-                                              placeholder="Enter email"/>
+                                              placeholder="Enter email"
+                                              onChange={updateEmail}
+                                    />
                                 </div>
                                 <div className="mb-3">
                                     <Formlabel text='Password'/>
                                     <Inputbox type="password"
                                               className="form-control"
-                                              placeholder="Enter password"/>
+                                              placeholder="Enter password"
+                                              onChange={updatePassword}
+                                    />
                                 </div>
 
-                                <Submitbtn text='Submit'/>
+                                <Submitbtn text='Submit' onPress={submitHandler}/>
                                 <div style={{paddingTop:'2vh'}}>
                                     <Submitbtn text='Forgot Password' secondary={true} />
+                                    <p style={{color:'red', fontSize:'150%'}}>{state.alerttxt}</p>
                                 </div>
 
                             </div>
